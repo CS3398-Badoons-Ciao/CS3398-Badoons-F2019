ff6c57ca5de8f270049085f25ad32e6eb80a405d
Bekahexe <54691399+Bekahexe@users.noreply.github.com>
2019-09-10 20:05:44 -0500

    Update Manager.java
    will fix later

--- Assignment/A3/ThreeSolidPrinciples/src/threesolid/Manager.java
+++ Assignment/A3/ThreeSolidPrinciples/src/threesolid/Manager.java
@@ -1,3 +1,6 @@
+//Using the 3 SOLID principles we seperated this class into its own file because, using the Open Close priciple,
+//this class is only responsible for these methods and no others.
+
 package threesolid;
 
 import java.awt.*;        // Using AWT container and component classes
