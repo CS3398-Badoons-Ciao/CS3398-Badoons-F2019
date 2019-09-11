642f306d9e0519da71a785b88c86dc35ae679dfc
AlexMorman <54646806+AlexMorman@users.noreply.github.com>
2019-09-10 20:03:45 -0500

    Updated Worker.exe
    Made comments for Worker.exe :)

--- Assignment/A3/ThreeSolidPrinciples/src/threesolid/Worker.java
+++ Assignment/A3/ThreeSolidPrinciples/src/threesolid/Worker.java
@@ -4,6 +4,12 @@ import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
 import java.io.*;
 
+//Alex Morman
+//Open / Close Principle: We seperated the Worker class from the IWorker and RWorker 
+//class so that changes to the Worker class would not affect the interfaces it implements
+
+//Single Responsibility Principle: By putting Worker class in its own file, the only reason
+//this file has to change is if something in the Worker class needs to change
 
 class Worker implements IWorker, RWorker{
  public void work() {
