ffcb3b95805444097e489964e017b27772aee8d2
BHubbard946 <54681365+BHubbard946@users.noreply.github.com>
2019-09-10 20:02:28 -0500

    Update ThreeSolidMain.java
    -Turn in for A3

--- Assignment/A3/ThreeSolidPrinciples/src/threesolid/ThreeSolidMain.java
+++ Assignment/A3/ThreeSolidPrinciples/src/threesolid/ThreeSolidMain.java
@@ -6,6 +6,17 @@ import java.io.*;
 
 //import threesolid.IWorker;
 
+/*This is the main file where the program runs from
+* All the extra classes and interfaces have been removed and separated into their files to allow multi able users to work on the program at the same time.
+* This has been done to implement the following principle...
+*  -Open Closed principle: This principle does not apply as nothing would be extending the main. To see this Principle in action refer to the interface classes.
+*  -Single Responsibility: By separating these files one user can reasonably manage the one class thus making the user responsible for that program alone. This all then follows this principle.
+*  -Interface segregation: This principle was not used in the main class because the main does not implement any interfaces, to see this in action refer to the interface class in IWorker.java
+*
+* -Bailey Hubbard
+*/
+
+
 
 public class ThreeSolidMain
 {   
@@ -39,43 +50,3 @@ public class ThreeSolidMain
    }
  }
 
-// interface segregation principle - good example
-//interface IWorker {
-// public void work();
-// public void eat();
-//}   
-
-//class Worker implements IWorker{
-// public void work() {
-//  // ....working
-// }
-//
-// public void eat() {
-//  //.... eating in launch break
-// }
-//}
-
-//class SuperWorker implements IWorker{
-// public void work() {
-//  //.... working much more
-// }
-//
-// public void eat() {
-//  //.... eating in launch break
-// }
-//}
-
-//class Manager {
-// IWorker worker;
-//
-// public void Manager() {
-//
-// }
-// public void setWorker(IWorker w) {
-//  worker=w;
-// }
-//
-// public void manage() {
-//  worker.work();
-// }
-//}
