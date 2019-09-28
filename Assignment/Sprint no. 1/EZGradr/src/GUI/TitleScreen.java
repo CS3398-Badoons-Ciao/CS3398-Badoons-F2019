94a87df9520c6290a38475ffefb426c7e4192e60
Charlie Walker <ctw60@txstate.edu>
2019-09-28 16:29:05 -0500

    Updating the code to compile within intellij

--- Assignment/Sprint no. 1/EZGradr/src/GUI/TitleScreen.java
+++ Assignment/Sprint no. 1/EZGradr/src/GUI/TitleScreen.java
@@ -6,21 +6,21 @@ import javafx.stage.*;
 import javafx.scene.layout.*;
 import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
+import java.io.IOException;
 
-public class TitleScreen extends Application {
+
+public class TitleScreen {
   
   private Scene sceneTitle ; // Stores the titleScreen created by this class.
   
-  public TitleScreen() {
-    sceneTitle = createScene();
+  public TitleScreen() throws IOException {
+    Parent root = FXMLLoader.load(this.getClass().getClassLoader().getResource("resources/sample.fxml")); // Loads the FXML
+    // TitleScene Creation
+    sceneTitle = new Scene(root,400,640); // Creates the Scene, "the window" of our program
   }
   
-  public Scene createScene() {
-    Parent root = FXMLLoader.load(getClass().getResource("/GUI/sample.fxml")); // Loads the FXML
-    // TitleScene Creation
-    Scene s = new Scene(root,400,640); // Creates the Scene, "the window" of our program
-    
-    return s;
+  public Scene getScene() throws IOException {
+    return sceneTitle;
   }
   
 }
--- Assignment/Sprint no. 1/EZGradr/src/Main.java
+++ Assignment/Sprint no. 1/EZGradr/src/Main.java
@@ -1,3 +1,8 @@
+package main;
+
+import GUI.*;
+
+import java.io.IOException;
 import javafx.application.*;
 import javafx.event.*;
 import javafx.stage.*;
@@ -5,27 +10,22 @@ import javafx.scene.layout.*;
 import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
 
-class Main extends Application {
-  
-  
-  private Scene titleScreen; // TitleScreen's GUI.
-  
-  // Main method for running this program.
-  public static void main(String[] args) {
-    launch(args); // Calls the launcher for the program.
-  }
-  
+public class Main extends Application {
+
   @Override
-  public void start(Stage main) throws IOException {    
-    
+  public void start(Stage main) throws IOException {
+
+    TitleScreen title = new TitleScreen(); // TitleScreen's GUI.
+
     main.setTitle("Grade Manager"); // Sets the top bar to "Grade Manager"
-    main.setScene(titleScene); // Sets the window the title window, "titleScene"
+    main.setScene(title.getScene()); // Sets the window the title window, "titleScene"
     main.show(); // Displays the current window.
      
   }
-  
-  public void init() {
-    titleScene = titleScene(); // initialize the title screen.
+
+  // Main method for running this program.
+  public static void main(String[] args) {
+    launch(args); // Calls the launcher for the program.
   }
 
 }
